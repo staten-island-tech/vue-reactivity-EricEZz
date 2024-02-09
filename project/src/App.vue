1f5c99c1b54e8c163b7c2047ad55678edab786da
EricEZz <zhangeric343@gmail.com>
2024-02-09 10:12:42 -0500

    dd

--- project/src/App.vue
+++ project/src/App.vue
@@ -8,14 +8,13 @@ import { RouterLink, RouterView } from 'vue-router'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
-
+  <h1>Pokemaines packs</h1>
   <RouterView />
 </template>
 
