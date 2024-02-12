6cd1dfe05a9c4d746bf8dec79d1aff8acaa1c4ea
EricEZz <zhangeric343@gmail.com>
2024-02-12 10:12:20 -0500

    ddd

--- project/src/App.vue
+++ project/src/App.vue
@@ -4,17 +4,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+ <h1>Pokemon Packs</h1>
 
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <h1>Pokemaines packs</h1>
+
   <RouterView />
 </template>
 
---
+++ project/src/components/PokeCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>{{ pack.name }}</h2>
+        <img :src="pack.img" alt="">
+        <h3>{{ clicked }}</h3>
+        <button @click="increment">Add to Cart</button>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
